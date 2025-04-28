456deef79fae57c16f889a82319ecf1a903764eb
Anthony Manning-Franklin <anthony.manning.franklin@gmail.com>
2025-04-28 23:31:10 +0800

    update never utils

--- mod.ts
+++ mod.ts
@@ -2,7 +2,7 @@ export * from "./callWith.ts";
 export * from "./compose.ts";
 export * from "./arr/index.ts";
 export * from "./Func.ts";
-export * from "./isWeakNever.ts";
+export * from "./never.ts";
 // export * from "./memoise.ts";
 export * from "./OfUnion.ts";
 // export * from "./withDebugLogging.ts";
