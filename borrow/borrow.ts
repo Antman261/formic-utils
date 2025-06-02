fea14a01a964e7109e4ed4f3ffe3e52715ddafa9
Anthony Manning-Franklin <anthony.manning.franklin@gmail.com>
2025-06-03 00:08:59 +0800

    fix

--- borrow/borrow.ts
+++ borrow/borrow.ts
@@ -41,7 +41,7 @@ export function toBorrow<T extends ObjWide>(v: T, kind: BorrowKind): Mutable<T> 
    * * 75ns to redefine existing property
    * * 105ns to use defineProperty for the first time
    */
-  if (isBorrow(v, kind)) {
+  if (!isBorrow(v, kind)) {
     /**
      * use defineProperty to prevent enumeration & rewriting.
      * uses statically defined mutableDescriptor to prevent redundant object instantiations & improve performance by ~10ns
